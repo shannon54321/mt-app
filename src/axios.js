5c06882c5ed062e01dffc84ba64d9c5f776daa82
shannon54321 <248128459@qq.com>
2020-04-08 01:04:10 +0800

    http

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://api.duyiedu.com';
+axios.defaults.baseURL = 'https://api.duyiedu.com';
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
