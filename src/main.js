3f4b35f753dcc46e1c3b5bd3f8ddd767170cb01f
shannon54321 <248128459@qq.com>
2020-04-08 02:20:43 +0800

    mainJs修改

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import VueMeta from 'vue-meta'
 import App from './App'
-import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+
 import '@/assets/css/main.css'
 import store from './store'
 
